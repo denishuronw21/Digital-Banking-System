7d1c90577cab4d97512cb940f0a8c99bb5920bb5
denishuronw21 <notbdenish@gmail.com>
2026-08-19 10:04:17 +0530

    Account Service & Properties Completed

--- account-service/src/main/java/com/banking/account_service/service/AccountEventConsumer.java
+++ account-service/src/main/java/com/banking/account_service/service/AccountEventConsumer.java
@@ -1,4 +1,59 @@
 package com.banking.account_service.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
 public class AccountEventConsumer {
+
+    private final AccountService accountService;
+
+    /**
+     * Consume transaction event from Kafka
+     * Credits Receiver Amount
+     */
+    @KafkaListener(topics = "transaction.completed")
+    public void consumeTransactionCompleted(@Payload Map<String,Object> payload){
+
+        try{
+            String receiverAccount = (String) payload.get("receiverAccountNumber");
+            BigDecimal amount = new BigDecimal(payload.get("amount").toString());
+
+            log.info("Crediting Account: {} amount: {}", receiverAccount,amount);
+            accountService.creditBalance(receiverAccount,amount);
+
+            } catch (Exception e){
+            log.error("Error crediting amount: {}", e.getMessage());
+        }
+
+    }
+
+    /**
+     * Consume fraud.detected event from Kafka
+     * Blocks the flagged account
+     * @param payload
+     */
+
+    @KafkaListener(topics = "fraud.detected")
+    public void consumeFraudDetected(@Payload Map<String,Object> payload) {
+
+        try{
+
+            String accountNumber = (String) payload.get("accountNumber");
+            log.info("Fraud Detected - blocking account: {}", accountNumber);
+
+            accountService.blockAccount(accountNumber);
+        } catch (Exception e) {
+            log.error("Error blocking amount: {}", e.getMessage());
+        }
+    }
+
 }
